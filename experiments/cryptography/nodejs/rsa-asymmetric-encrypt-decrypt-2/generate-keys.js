8244116c6b2228d2abd3d413b93a6af2ff558c63
Robert Wagner <me@robert.com.br>
2021-06-12 22:25:05 +0200

    added asymmetric public/private key gen in node.js

---
+++ experiments/cryptography/nodejs/rsa-asymmetric-encrypt-decrypt-2/generate-keys.js
@@ -0,0 +1,23 @@
+const { generateKeyPairSync } = require('crypto');
+const { writeFileSync } = require('fs');
+
+const generateKeys = () => {
+    return generateKeyPairSync("rsa", {
+        modulusLength: 4096,
+        publicKeyEncoding: {
+            type: "pkcs1",
+            format: "pem",
+        },
+        privateKeyEncoding: {
+            type: "pkcs1",
+            format: "pem",
+            cipher: "aes-256-cbc",
+            passphrase: "",
+        }
+    });
+};
+
+const { privateKey, publicKey } = generateKeys();
+
+writeFileSync("private.pem", privateKey);
+writeFileSync("public.pem", publicKey);
