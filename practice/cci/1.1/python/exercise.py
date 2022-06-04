f66dea2e2a820f1f3d873fd95e002733347ad62e
Robert Souza <me@robert.com.br>
2022-06-04 15:13:41 +0200

    added 1.1

---
+++ practice/cci/1.1/python/exercise.py
@@ -0,0 +1,15 @@
+# Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+# cannot use additional data structures?
+# Hints: #44, #117, #132
+
+
+def isUnique(text):
+    for index, a in enumerate(text):
+        for b in text[index+1:]:
+            if a == b:
+                return False
+    return True
+
+# Unit tests
+print(isUnique("abcde") == True)
+print(isUnique("xyzxa") == False)
